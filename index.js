abf05926f9f67f0fb96f369d7207f1c1eeba0fb3
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2020-08-13 00:34:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const Guild = new Discord.Guild();
+const prefix = process.env.prefix
 
 client.on('ready', () => {
-  client.user.setStatus('online', "n-help")
+  client.user.setStatus('online', "auk-?")
   client.user.setPresence({
         game: {
             name: 'auk-?',
@@ -16,6 +17,11 @@ client.on('message', message => {
   var sender = message.author.username;
   var msglow = message.content.toLowerCase();
   
+  if (msglow.includes(prefix)) {
+   if (msglow.includes("invite")) {
+   message.channel.send("https://discord.com/api/oauth2/authorize?client_id=743150383496429649&scope=bot&permissions=2048")
+   } 
+  }
 })
 
 client.login(process.env.token);
