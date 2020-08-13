2fe9904b74ccb85a4826ff832aeda045e66a91bb
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2020-08-14 01:20:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ client.on('message', message => {
 	const collectormessageplace = new Discord.MessageCollector(message.channel, m => m.author.id == message.author.id, { time: 100 });
         collectormessageplace.on('collect', message => {
 		var msglow = message.content.toLowerCase();
+		console.log(place[msglow] == undefined);
         	if (place[msglow] == undefined) {
 			message.channel.send("error 404 : not found!")
 			collectormessageplace.stop();
