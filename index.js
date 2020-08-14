0e5499d00f4e54a7e594fdf15a0fae93c7c20860
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2020-08-14 08:55:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ client.on('message', message => {
         break;
       case "place":
 	message.channel.send("type the name of the place").then(() => {
-	message.channel.awaitMessages(m => m.author.id == message.author.id, { max: 1, time: 10, errors: ['time'] })
+	message.channel.awaitMessages(m => m.author.id == message.author.id, { max: 1, time: 100, errors: ['time'] })
 		.then(collected => {
 			var msglow = message.content.toLowerCase();
 			console.log(place[msglow] == undefined);
